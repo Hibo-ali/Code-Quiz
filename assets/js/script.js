d2ce4a5a1eae2e5350c2d4093b0151650164969d
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-17 15:44:29 -0500

    added time values and functionality for restart button

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,34 @@ continue_btn.onclick = ()=>{
   startTimer(10); //calling startTimer function
   startTimerLine(0); //calling startTimerLine function
 }
+
+
+let timeValue =  10;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if restartQuiz button clicked
+restart_quiz.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    result_box.classList.remove("activeResult"); //hide result box
+    timeValue = 10; 
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuestions(que_count); //calling showQuestions function
+    queCounter(que_numb); //passing que_numb value to queCounter
+    clearInterval(counter); //clear counter
+    clearInterval(counterLine); //clear counterLine
+    startTimer(timeValue); //calling startTimer function
+    startTimerLine(widthValue); //calling startTimerLine function
+    timeText.textContent = "Time Left"; //change the text of timeText to Time Left
+    next_btn.classList.remove("show"); //hide the next button
+}
